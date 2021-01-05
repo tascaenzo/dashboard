c8bcc992b65a0276d4770363cbdfe8bac9d2b16a
enzo <enzo@enzos-iMac-Pro.local>
2021-01-06 00:52:35 +0100

    fix

--- src/store/auth/dist/actions.type.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.REFRESH = exports.LOGOUT = exports.LOGIN = void 0;
-exports.LOGIN = "LOGIN";
-exports.LOGOUT = "LOGOUT";
-exports.REFRESH = "LOGOUT";
